26c6fe5addd3524a77ee6daec640fb0f65e32254
ViaticusRex <vdelacruz417@gmail.com>
2024-07-19 18:20:11 -0400

    finished draft of TopBar w/ aligned items + gradient

--- src/layout/TopBar.jsx
+++ src/layout/TopBar.jsx
@@ -5,12 +5,13 @@ import '../styles/TopBar.css'
 
 //import img or icons
 import shoppingCartIcon from '../assets/shopping_cart.svg'
+import logo from '../assets/sprocket_science_logo.png'
 
 const TopBar = () => {
   return (
     <div className='topbar__container'>
       <div className="logo">
-        Logo Here
+      <img src={logo} alt="Logo img" />
       </div>
       <div className="shopping_cart_icon">
         <img src={shoppingCartIcon} alt="Logo img" />
