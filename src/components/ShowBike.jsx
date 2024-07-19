85deed1d0667e9dfab4c9fee7b8e037546f4c602
MiloCo86 <cpaez@pursuit.org>
2024-07-19 00:40:42 -0400

    added template components ans css for showBike and AddBike

---
+++ src/components/ShowBike.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../styles/ShowBike.css'
+
+const ShowBike = () => {
+  return (
+    <div className='showBike__container'>
+      
+    </div>
+  )
+}
+
+export default ShowBike
