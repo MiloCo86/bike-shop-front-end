c14ef401c8d687772a877c3cacb24307ec263e29
Brendan O'Brien <bobrien@pursuit.org>
2024-07-21 17:36:48 -0400

    changes background image from react to css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,6 @@ import '../styles/Home.css';
 const Home = () => {
   return (
     <div className='home__container'>
-      <img src="https://pedalshift.net/wp-content/uploads/2024/04/pedalshift-361-science-of-cycling.jpeg" />
     </div>
   )
 }
