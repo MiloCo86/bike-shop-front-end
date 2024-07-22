c9443b41673a1a3f9400149e0e4a072fd7d2893d
Brendan O'Brien <bobrien@pursuit.org>
2024-07-22 02:14:41 -0400

    home page layout draft

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,20 @@ import '../styles/Home.css';
 
 const Home = () => {
   return (
-    <div className='home__container'>
+    <div className='home__main'>
+      <div className='home__container'>
+        <div className="home__main-content">
+        Main Content
+        </div>
+        <div className="home__testimonials">
+          <div className="home__testimonial-box-1">Testimonial 1</div>
+          <div className="home__testimonial-box-2">Testimonial 2</div>
+          <div className="home__testimonial-box-3">Testimonial 3</div>
+        </div>
+        </div>
+        <div className="home__sidebar">
+          Sidebar
+      </div>
     </div>
   )
 }
