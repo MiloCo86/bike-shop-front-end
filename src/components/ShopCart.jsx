6c5771c65dc9a829f0728c9bd571ebd3f6229e34
MiloCo86 <cpaez@pursuit.org>
2024-07-22 17:47:37 -0400

    shopCartElement and basic functionality added

--- src/components/ShopCart.jsx
+++ src/components/ShopCart.jsx
@@ -1,11 +1,67 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom';
+import { useState,useEffect } from 'react';
 
+//components
+import ShopCartItem from './ShopCartItem';
+
+//icons
+
+
+//css
 import '../styles/ShopCart.css'
 
+
+
+
 const ShopCart = () => {
+
+  const [shopCartElements, setShopCartElements] = useState([])
+  const [sortedElements, setSortedElements] = useState([])
+  const [total, setTotal] =useState(0)
+  const API = import.meta.env.VITE_SHOPCART_URL
+
+  useEffect(()=>{
+    
+    fetch(API)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        setShopCartElements(res)
+      })
+      .catch(err => console.error(err))
+  },[])
+
+  useEffect(()=>{
+    setTotal(shopCartElements.reduce((accumulator, currentValue) => accumulator + (currentValue.price * currentValue.quantity),
+        0,))
+  },[shopCartElements])
+
+  useEffect(()=>{
+    setSortedElements(shopCartElements.sort((a,b)=> a.txn_id - b.txn_id))
+  },[shopCartElements])
+
   return (
-    <div>
-      
+    <div className='shopCart__container'>
+        <h2>SHOP CART</h2>
+        <form className='shopCart__form' action="">
+          <div className="shopCart__items">
+          { sortedElements.map((shopItem, idx) =>{
+            return (
+                <div key={idx}>
+                    <ShopCartItem shopItem={shopItem} />
+                </div>
+              )
+            })
+          }
+          </div>
+          <div className="shopCart__total">
+            <h3>TOTAL:</h3>
+            <div className="shopCart__totalPrice"> $ {total}</div>
+          </div>
+          <button className='shopCart__btn'>Checkout</button>
+          <div className="shopCart__empty">empty ShopCart</div>
+        </form>
     </div>
   )
 }
---
+++ src/components/ShopCartItem.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+//icons
+import deleteIcon from '../assets/delete_75dp.svg'
+
+//css
+import '../styles/ShopCartItem.css'
+
+const ShopCartItem = ({shopItem}) => {
+
+    let navigate = useNavigate();
+
+    const [item, setItem] = useState({
+        id: shopItem.id,
+        make: shopItem.make,
+        model: shopItem.model,
+        price: shopItem.price,
+        img_url: shopItem.img_url,
+        quantity: shopItem.quantity
+    })
+
+    const API = import.meta.env.VITE_SHOPCART_URL
+        
+    const handleChange = (e) => {
+        if(e.target.value>=1){
+            setItem((prevState) => {
+                return { ...prevState, [e.target.name]: e.target.value }
+            })
+
+            fetch(`${API}/${shopItem.txn_id}`, {
+                method: "PUT",
+                body: JSON.stringify(item),
+                headers: {
+                  "Content-Type": "application/json"
+                }
+              })
+                .then(res => res.json())
+                .then(res => {
+                  if(res.error){
+                    alert(res.error)
+                  }
+                })
+                .catch(err => console.error(err))
+        }
+        
+    }
+
+    const handleDelete = () => {
+        fetch(`${API}/${shopItem.txn_id}`, {
+          method: 'DELETE',
+        })
+        .then(res => res.json())
+        .then(res => navigate('/shopcart'))
+        .catch(err => console.error(err))
+      };
+    
+
+  return (
+    <div className='item__container' >
+        <label htmlFor="quantity">Qty</label>
+        <input type="number" value={item.quantity} name='quantity' onChange={handleChange}/>
+        <img className='item_miniIMG' src={item.img_url} alt="" />
+        <p className='item__name'>{item.make} {item.model}</p>
+        <p className="item__price">${item.price}</p>
+        <img className='item_delIcon' src={deleteIcon} alt="" onClick={handleDelete} />
+    </div>
+  )
+}
+
+export default ShopCartItem
