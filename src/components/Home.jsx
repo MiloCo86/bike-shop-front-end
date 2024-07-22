47ce74e4cab70f654bc6a33e03a46019f66b3ade
Brendan O'Brien <bobrien@pursuit.org>
2024-07-22 17:38:14 -0400

    adds photos to assets and trying to add them ot home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -45,7 +45,16 @@ const Home = () => {
     <div className='home__main'>
       <div className='home__container'>
         <div className="home__main-content">
-        Main Content
+          <img src='/src/assets/istockphoto-825854604-1024x1024.jpg'/>
+          <h1>Welcome to Sprocket Science</h1>
+          <h2>Buying a bike should be simple</h2>
+        <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Ut nulla tempus bibendum diam tempor. 
+          Placerat convallis dolor diam consectetur quam penatibus porta leo. Inceptos sagittis quis 
+          vivamus ipsum montes efficitur. Vitae netus montes fames conubia hac. Class tempus placerat congue 
+          semper finibus in arcu. In suspendisse risus velit tempor volutpat dapibus lectus parturient. 
+          Molestie molestie magnis eleifend cubilia sapien lobortis. Luctus fames vivamus quam ac lacinia 
+          venenatis eu quisque. Metus litora vehicula nulla; quisque a urna ad. Sem sed hendrerit nulla 
+          ante mus lobortis. Vulputate condimentum egestas cursus dictum dignissim sollicitudin feugiat ut.</p>
         </div>
         <div className="home__testimonials">
           <div className="home__testimonial-box">Testimonial 1</div>
